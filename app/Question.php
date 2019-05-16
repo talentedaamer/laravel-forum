818529e39bb49783b25b8c51c33d5a42d9f3bcf1
Aamer Shahzad <a.shahzad@airtango.pk>
2019-05-16 11:01:39 +0500

    questions crud, auth, custom css, add telescope for debugging

--- app/Question.php
+++ app/Question.php
@@ -21,4 +21,46 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+    
+    public function getUrlAttribute()
+    {
+        return route( 'questions.show', $this->id );
+    }
+    
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+    
+    public function getStatusAttribute()
+    {
+        if ( $this->answers > 0 ) {
+            if ( $this->accepted_answer ) {
+                return 'ans-is-accepted';
+            }
+            return 'is-ansed';
+        }
+        
+        return 'is-unansed';
+    }
+    
+    public function getVoteStatusAttribute()
+    {
+        if ( $this->votes > 0 ) {
+            return 'has-votes';
+        } elseif ( $this->votes < 0 ) {
+            return 'neg-votes';
+        } else {
+            return 'no-votes';
+        }
+    }
+    
+    public function getViewStatusAttribute()
+    {
+        if ( $this->views > 0 ) {
+            return 'has-views';
+        }
+    
+        return 'no-views';
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -4,18 +4,27 @@
     <div class="container">
         @foreach( $questions as $question )
         <div class="card mb-3">
-            <div class="card-header">
-                <span class="badge badge-success">Votes : {{$question->votes}}</span>
-                <span class="badge badge-primary">Views : {{$question->views}}</span>
-                <span class="badge badge-warning">Answers : {{$question->answers}}</span>
-                {{ $question->title }}
-            </div>
             <div class="card-body">
                 <div class="media">
                     <div class="media-body">
+                        <h4><a href="{{ $question->url }}">{{ $question->title }}</a></h4>
                         {{ Str::limit( $question->body, 255 ) }}
                     </div>
                 </div>
+            </div>
+            <div class="card-footer">
+                <ul class="q-status list-inline mb-0 mr-2 d-inline-block">
+                    <li class="q-status-votes list-inline-item">
+                        <span class="badge badge-secondary badge-md {{ $question->VoteStatus }}">{{$question->votes}} <span class="d-block">{{ Str::plural( 'Vote', $question->votes) }}</span></span>
+                    </li>
+                    <li class="q-status-views list-inline-item">
+                        <span class="badge badge-primary badge-md {{ $question->ViewStatus }}">{{$question->views}} <span class="d-block">{{ Str::plural( 'View', $question->views) }}</span></span>
+                    </li>
+                    <li class="q-status-ans list-inline-item">
+                        <span class="badge badge-secondary badge-md {{ $question->status }}">{{$question->answers}} <span class="d-block">{{ Str::plural( 'Answer', $question->answers) }}</span></span>
+                    </li>
+                </ul>
+                <p class="d-inline-block mb-0">Asked By <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> - {{ $question->created_date }}</p>
             </div>
         </div>
         @endforeach
